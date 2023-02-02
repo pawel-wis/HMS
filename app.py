e4d3209afa70845883ec1e5a4a119e45be719211
pawewisn <pawel.wis.me@gmail.com>
2023-02-02 15:31:18 -0500

    render title into template

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
+APP_TITLE = "HMS"
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title=APP_TITLE)
 
 
 if __name__ == '__main__':
