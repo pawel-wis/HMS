839c225e91d1acb649e97635ed36ed2cb4aa024a
pawewisn <pawel.wis.me@gmail.com>
2023-02-04 13:24:47 -0500

    sensor manager

---
+++ sensor_manager/SensorManager.py
@@ -0,0 +1,16 @@
+class SensorManager:
+    # initialize all sensor libraries
+    def __init__(self):
+        pass
+
+    # get all information from all sensors
+    def _get_sensors_data(self):
+        pass
+
+    # return dictionary with all sensors valeus
+    def get_data(self):
+        pass
+
+    # Async lcd viewer
+    def start_view_data_lcd(self):
+        pass
